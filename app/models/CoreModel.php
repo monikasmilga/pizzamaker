06d02d47d8102da3f285be709794c462fa9ced90
aivarasvain <aivarasvainius11@gmail.com>
2017-05-08 15:15:43 +0300

    Added comments

--- app/models/CoreModel.php
+++ app/models/CoreModel.php
@@ -8,11 +8,19 @@ use Ramsey\Uuid\Uuid;
 
 class CoreModel extends Model
 {
+
+    /**
+     * Incrementing is set to false
+     *
+     * @var bool
+     */
     public $incrementing = false;
 
     use SoftDeletes;
 
-
+    /**
+     * Automatically generates and adds UUID to model
+     */
     protected static function boot() : void {
         parent::boot();
         static::creating(function($model) {
