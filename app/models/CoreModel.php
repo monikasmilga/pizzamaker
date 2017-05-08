2c819f54cec8a5eb88daf4a097fc1f85cd10b2ed
aivarasvain <aivarasvainius11@gmail.com>
2017-05-08 15:13:58 +0300

    Function returns void by php 7

--- app/models/CoreModel.php
+++ app/models/CoreModel.php
@@ -3,12 +3,17 @@
 namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class CoreModel extends Model
 {
     public $incrementing = false;
+
     use SoftDeletes;
-    protected static function boot() {
+
+
+    protected static function boot() : void {
         parent::boot();
         static::creating(function($model) {
             $model->{$model->getKeyName()} = Uuid::uuid4();
