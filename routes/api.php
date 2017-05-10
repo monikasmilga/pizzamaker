765deac15e80881c3d5b6089936b639330f0cd51
monikasmilga <tuuli705@gmail.com>
2017-05-10 14:06:22 +0300

    Created api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'api/v1'], function () {
+
+    Route::group(['prefix' => 'pizzas'], function () {
+
+        Route::get('/', ['as' => 'api.pizzas.index', 'uses' => 'DTPizzasController@index']);
+
+        Route::get('/create', ['as' => 'api.pizzas.create', 'uses' => 'DTPizzasController@create']);
+        Route::post('/create', ['uses' => 'DTPizzasController@store']);
+
+        Route::group(['prefix' => '{id}'], function () {
+
+            Route::get('/edit', ['as' => 'api.pizzas.edit', 'uses' => 'DTPizzasController@edit']);
+            Route::post('/edit', ['uses' => 'DTPizzasController@update']);
+
+            Route::get('/', ['uses' => 'DTPizzasController@show']);
+            Route::delete('/', ['as' => 'api.pizzas.delete', 'uses' => 'DTPizzasController@destroy']);
+        });
+    });
+});
