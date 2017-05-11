ff11b7ea9514bbfbedb7aa277d8272e61725d819
monikasmilga <tuuli705@gmail.com>
2017-05-11 08:44:31 +0300

    Edited routes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'api/v1'], function () {
+Route::group(['prefix' => 'v1'], function () {
 
     Route::group(['prefix' => 'pizzas'], function () {
 
